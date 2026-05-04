85728d7961d7aea6e39764ceb79358d9f59cbfcb
yashakib-dev <yashakib.dev@gmail.com>
2026-05-04 17:01:26 +0600

    Login issue

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,7 +1,4 @@
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
 "use client";
-
 import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { FcGoogle } from "react-icons/fc";
